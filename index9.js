1ca3be5af87dbf7aa88785c44a55f990ae53fafb
yaxiong_liu <yaxiong_liu@crfchina.com>
2017-02-23 20:15:07 +0800

    addon
    tbc

--- index9.js
+++ index9.js
@@ -3,25 +3,27 @@
 var promise = new Promise(function(resolve,reject){
 	resolve("finished")
 })
+var pro_1 = Promise.resolve(promise)
+/**
+ * {Promise { 'finished' }}
+ * {promise包装之后还是promise}
+ */
 
 var thenable = {
   	then: function(resolve, reject) {
     	resolve(42);
   	}
 };
-
 var pro_2 = Promise.resolve(thenable);
 pro_2.then(function(value) {
   	console.log(value);  
 });
-
-
-
 /**
- * {Promise { 'finished' }}
+ * {42}
+ * {thenable的对象，包装之后会立即执行resolve}
  */
-var pro_1 = Promise.resolve(promise) 
 
-var pro_2 = Promise.resolve(thenable) 
+ 
 
-console.log(pro_2.then())
+
+
