751a42295343fe704acff4c9a2222a441b02e56d
yaxiong_liu <yaxiong_liu@crfchina.com>
2017-02-27 20:10:59 +0800

    es6
    tbc

--- variable-expansion/index04.js
+++ variable-expansion/index04.js
@@ -1,16 +1,32 @@
 
 "use strict"
 
-//遍历字符串
+//	遍历字符串
 for (let codePoint of 'foo') {
   console.log(codePoint)
 }
 
+//	startsWith() endsWith() includes() 后面跟的参数是开始索引
 var s = 'Hello world!';
+s.startsWith('world', 6)	// true
+s.endsWith('Hello', 5) 		// true
+s.includes('Hello', 6) 		// false
 
-s.startsWith('Hello') // true
-s.endsWith('!') // true
-s.includes('o') // true
-
-//at()
+//	at()
 console.log( 'abc'.charAt(0) )
+
+//	repeat()
+
+var item_4 = 'hello'.repeat(2) // "hellohello"
+console.log( item_4 )
+
+
+
+
+
+
+
+
+
+
+
