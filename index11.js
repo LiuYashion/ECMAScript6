95d65a6def2f73d113517eac013d5dc3b98f9f4e
yaxiong_liu <yaxiong_liu@crfchina.com>
2017-02-27 11:12:05 +0800

    add
    tbc

--- index11.js
+++ index11.js
@@ -1,6 +1,31 @@
 
-var f = () => console.log("LiuYashion")
+//var f = () => console.log("LiuYashion")
+//
+//console.log("step: 1")
+//Promise.resolve().then(f)
+//console.log("step: 2")
 
-console.log("---->: 1")
-Promise.resolve().then(f)
-console.log("---->: 2")
+/*
+ * step:1
+ * step:2
+ * LiuYashion
+ */
+
+const f = () => console.log('now');
+(
+  () => new Promise(
+    resolve => resolve(f())
+  )
+)();
+console.log('next');
+
+
+
+
+
+
+
+
+
+
+
